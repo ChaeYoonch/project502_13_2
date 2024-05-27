894b0f2cf7680a199a8809f8ed153a89bb561a27
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-27 14:54:58 +0900

    Templates

--- src/main/java/org/choongang/template/Templates.java
+++ src/main/java/org/choongang/template/Templates.java
@@ -2,17 +2,63 @@ package org.choongang.template;
 
 import org.choongang.global.constants.Menu;
 import org.choongang.template.main.MainTpl;
+import org.choongang.template.member.JoinTpl;
+import org.choongang.template.member.LoginTpl;
+import org.choongang.template.member.MypageTpl;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Templates {
+    private static Templates instance;
+    private Map<Menu, Template> tpls;
+
+    private Templates() {
+        tpls = new HashMap<>();
+    }
+
+    public static Templates getInstance() {
+        if (instance == null) { // instance 가 null 인 경우
+            instance = new Templates();
+        }
+
+        return instance;
+    }
+
     public void render(Menu menu) {
-        Template tpl = null;
+        /* Template tpl = null;
         switch (menu) {
             case JOIN:
             case LOGIN:
             case MYPAGE:
             default: tpl = new MainTpl();
+        } */
+        System.out.println(find(menu).getTpl());
+    }
+
+    public Template find(Menu menu) {
+        Template tpl = tpls.get(menu);
+        if (tpl != null) {
+            return tpl;
         }
 
-        System.out.println(tpl.getTpl());
+        switch (menu) {
+            case JOIN: tpl = new JoinTpl(); break;
+            case LOGIN: tpl = new LoginTpl(); break;
+            case MYPAGE: tpl = new MypageTpl(); break;
+            default: tpl = new MainTpl();
+        }
+
+        tpls.put(menu, tpl);
+
+        return tpl;
+    }
+
+    public String line() {
+        return "------------------------------\n";
+    }
+
+    public String doubleline() {
+        return "==============================\n";
     }
 }

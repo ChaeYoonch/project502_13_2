c70e9edcdaec1df816cb5954381e7854642dfc4d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-27 16:03:08 +0900

    회원 가입 기능

--- src/main/java/org/choongang/member/controllers/JoinController.java
+++ src/main/java/org/choongang/member/controllers/JoinController.java
@@ -2,6 +2,7 @@ package org.choongang.member.controllers;
 
 import org.choongang.global.AbstractController;
 import org.choongang.global.constants.Menu;
+import org.choongang.main.MainRouter;
 import org.choongang.template.Templates;
 
 /**
@@ -11,7 +12,41 @@ public class JoinController extends AbstractController {
 
     @Override
     public void show() {
-
         Templates.getInstance().render(Menu.JOIN);
+    }
+
+    @Override
+    public void prompt() {
+        /* System.out.print("아이디 : ");
+        String userId = sc.nextLine();
+        System.out.println(userId); */
+        String userId = promptWithValidation("아이디 (6자리 이상) : ", s -> s.length() >= 6);
+
+        String userPw = promptWithValidation("비밀번호 (8자리 이상) : ", s -> s.length() >= 8);
+
+        String confirmPw = promptWithValidation("비밀번호 확인 : ", s ->
+        {
+            boolean match = s.equals(userPw);
+            if (!match) {
+                System.err.println("\n비밀번호가 일치하지 않습니다.");
+            }
+
+            return match;
+        });
+
+        String userNm = promptWithValidation("회원명 : ", s -> !s.isBlank());
+
+        RequestJoin form = RequestJoin.builder()
+                .userId(userId)
+                .userPw(userPw)
+                .confirmPw(confirmPw)
+                .userNm(userNm)
+                .build();
+
+        // 회원 가입 처리 ...
+        System.out.println(form);
+
+        // 회원 가입 성공 시 -> 로그인 화면으로 이동
+        MainRouter.getInstance().change(Menu.LOGIN);
     }
 }

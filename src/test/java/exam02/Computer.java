de15b10e6dafc770cf8670725ed72ef9239b5da7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-30 10:20:30 +0900

    다형성 이용 예시2

---
+++ src/test/java/exam02/Computer.java
@@ -0,0 +1,18 @@
+package exam02;
+
+public class Computer {
+
+    public void monitor() {
+
+    }
+
+    public void touchScreen() {}
+
+    public void body() {
+
+    }
+
+    public void keyboard() {}
+
+    public void mouse() {}
+}

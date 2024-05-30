5e1e3da195ebcc455afeabcbd2528f4191b449c6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-30 10:57:03 +0900

    다형성 이용 예시3 : SOLID

--- src/test/java/exam03/Desktop.java
+++ src/test/java/exam03/Desktop.java
@@ -18,5 +18,9 @@ public class Desktop {
 
     public void run() { // 내부에서 작동
         // monitor, body, keyboard, mouse 객체를 활용
+        // Desktop 객체는 구성 객체 (monitor, body, keyboard, mouse) 의 변화에 영향을 받는 관계 -> 통제가 필요 (메서드 내에서 통제)
+        // -> 캡슐화 : 폐쇄 (메서드 안에 있어야만 가능 = 코드가 안에 있기 때문에)
+        // 개방 폐쇄 : 확장에는 열려있고, 변화에는 닫혀있음 => 전략 패턴
+        body.run();
     }
 }
